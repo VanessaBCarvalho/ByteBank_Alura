7109eb323232d905c9d73effd7352c32ef0e2063
Vanessa <vanessa_bcarvalho@hotmail.com>
2026-03-03 17:08:09 -0300

    worker

--- script/scripts.js
+++ script/scripts.js
@@ -1,23 +1,18 @@
+import imprimeCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.getElementById('graficoDolar');
 
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: [],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Dólar',
+        data: [],
         borderWidth: 1
       }]
     },
     
-  });
-  setInterval(() => conectaAPI(), 5000)
-  async function conectaAPI() {
-    const conecta = await fetch ("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
-  }
+  })
 
  function geraHorario(){
   let data = new Date();
@@ -26,4 +21,19 @@ const graficoParaDolar = new Chart(graficoDolar, {
   return horario;
  }
 
- geraHorario();
+ function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {dataset.data.push(dados);})
+  
+ grafico.update();
+ }
+
+ let workerDolar = new Worker('./script/workers/workerDolar.js');
+ workerDolar.postMessage('usd');
+
+ workerDolar.addEventListener("message", event => {
+  let tempo = geraHorario();
+  let valor = event.data.ask;
+  imprimeCotacao("dolar", valor);
+  adicionarDados(graficoParaDolar, tempo, valor);
+ })
